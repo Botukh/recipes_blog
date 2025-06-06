3b1c65aefc98f57218ed4812dccc631fe344c13b
Iuliia Botukh <u.botuh@yandex.ru>
2025-06-06 15:18:35 +0400

    29

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -99,7 +99,7 @@ DJOSER = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
