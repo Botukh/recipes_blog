0a20604a273cf9633116940eecac876c50c082c1
Iuliia Botukh <u.botuh@yandex.ru>
2025-06-09 19:51:16 +0400

    3.8

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,13 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/', include('djoser.urls')),
-    path('api/', include('djoser.urls.authtoken')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("api/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.authtoken")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
