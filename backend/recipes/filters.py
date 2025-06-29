107f7f8686a8386e133a754203fc8dc8ed8cc658
Iuliia Botukh <u.botuh@yandex.ru>
2025-06-29 22:09:00 +0400

    13.5

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -16,13 +16,13 @@ class RecipeFilter(filters.FilterSet):
     )
     author = filters.NumberFilter(field_name='author__id')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_in_shopping_carts = filters.BooleanFilter(
         method='filter_is_in_shopping_carts',
     )
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_carts')
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
@@ -30,7 +30,7 @@ class RecipeFilter(filters.FilterSet):
             return queryset.filter(favorited_by__user=user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
+    def filter_is_in_shopping_carts(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(in_shopping_carts__user=user)
