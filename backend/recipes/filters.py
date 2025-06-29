5a2188bd28788117a587135205695c19f9907392
Iuliia Botukh <u.botuh@yandex.ru>
2025-06-29 23:09:57 +0400

    13.8

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,7 +1,8 @@
 from django_filters import rest_framework as filters
+from django.db.models import Exists, OuterRef
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag
+from recipes.models import Recipe, Tag, ShoppingCart
 
 
 class IngredientSearchFilter(SearchFilter):
@@ -33,11 +34,20 @@ class RecipeFilter(filters.FilterSet):
             return queryset.none()
         return queryset
 
-    def filter_is_in_shopping_carts(self, queryset, name, value):
+    def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
-        true_values = (True, '1', 1, 'true', 'True')
-        if value in true_values and user.is_authenticated:
-            return queryset.filter(in_shopping_carts__user=user).distinct()
-        if (value == '' or value is None) and user.is_authenticated:
-            return queryset.none()
+        if not user.is_authenticated:
+            return queryset
+        if value or value == '' or value is None:
+            sub_qs = ShoppingCart.objects.filter(
+                user=user,
+                recipe=OuterRef('pk'),
+            )
+            return (
+                queryset
+                .annotate(_in_cart=Exists(sub_qs))
+                .filter(_in_cart=True)
+                .order_by('id')
+                .distinct('id')
+            )
         return queryset
