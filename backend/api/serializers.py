5ededc055651e7a9be1af87e2135f84c94ba8155
Iuliia Botukh <u.botuh@yandex.ru>
2025-07-16 20:14:13 +0400

    21

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -90,13 +90,14 @@ class RecipeReadSerializer(serializers.ModelSerializer):
     )
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField()
 
     class Meta:
         model = Recipe
         fields = (
             'id', 'tags', 'author', 'ingredients',
             'is_favorited', 'is_in_shopping_cart',
-            'name', 'image', 'text', 'cooking_time'
+            'name', 'image', 'text', 'cooking_time', 'url'
         )
         read_only_fields = fields
 
