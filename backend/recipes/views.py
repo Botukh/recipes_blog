ff328cfd9f33507d859df5025e7e4a717738e088
Iuliia Botukh <u.botuh@yandex.ru>
2025-07-18 12:29:40 +0400

    24.8

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect
+from django.http import Http404
 
 from recipes.models import Recipe
 
 
 def short_link_redirect(request, recipe_id):
-    """Редирект с короткой ссылки на страницу рецепта."""
-    recipe = get_object_or_404(Recipe, id=recipe_id)
-    return redirect(f'/recipes/{recipe.id}/')
+    """Редирект на страницу рецепта по короткой ссылке."""
+    if not Recipe.objects.filter(id=recipe_id).exists():
+        raise Http404(f'Рецепт с id={recipe_id} не найден.')
+    return redirect(request.build_absolute_uri(f'/recipes/{recipe_id}'))
