9936ed63028409835d1a337ea805c7b4608a509a
Iuliia Botukh <u.botuh@yandex.ru>
2025-07-18 13:12:21 +0400

    24.9

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import redirect
-from django.http import Http404
+from django.core.exceptions import ValidationError
 
 from recipes.models import Recipe
 
 
 def short_link_redirect(request, recipe_id):
-    """Редирект на страницу рецепта по короткой ссылке."""
+    """Редирект с короткой ссылки на страницу рецепта."""
     if not Recipe.objects.filter(id=recipe_id).exists():
-        raise Http404(f'Рецепт с id={recipe_id} не найден.')
-    return redirect(request.build_absolute_uri(f'/recipes/{recipe_id}'))
+        raise ValidationError(f'Рецепт с id={recipe_id} не найден.')
+    return redirect(f'/recipes/{recipe_id}/')
