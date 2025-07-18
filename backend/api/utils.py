fa55646dfd9d90f0185b00505f3d6c6b59cb2745
Iuliia Botukh <u.botuh@yandex.ru>
2025-07-18 14:17:45 +0400

    25.2

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,28 +1,25 @@
 from datetime import date
+
 from django.template.loader import render_to_string
+from django.http import FileResponse
 from django.db.models import Sum
 
-from recipes.models import RecipeIngredient
+from recipes.models import Recipe, RecipeIngredient
 
 
 def generate_shopping_list(user):
     """Функция для создания списка покупок."""
-    recipes_qs = RecipeIngredient.objects.filter(
-        recipe__in_shoppingcarts__user=user
-    ).select_related('ingredient', 'recipe')
+    recipes_qs = Recipe.objects.filter(in_shoppingcarts__user=user)
 
     ingredients = (
-        recipes_qs
+        RecipeIngredient.objects
+        .filter(recipe__in=recipes_qs)
         .values('ingredient__name', 'ingredient__unit')
         .annotate(total_amount=Sum('amount'))
         .order_by('ingredient__name')
     )
 
-    recipes = (
-        recipes_qs
-        .values_list('recipe__name', 'recipe__author__username')
-        .distinct()
-    )
+    recipes = recipes_qs.values_list('name', 'author__username')
 
     context = {
         'date': date.today(),
@@ -30,4 +27,11 @@ def generate_shopping_list(user):
         'recipes': recipes,
     }
 
-    return render_to_string('shopping_list.txt', context)
+    content = render_to_string('shopping_list.txt', context)
+
+    return FileResponse(
+        content,
+        as_attachment=True,
+        filename='shopping_list.txt',
+        content_type='text/plain'
+    )
